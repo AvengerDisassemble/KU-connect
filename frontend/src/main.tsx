0bed9e6bf2958e00dfb59f3f178fb7772318d51c
Sunthorn Kompita <sunthorn.ko@ku.th>
2025-09-28 19:25:59 +0700

    feat(profile): add edit function to tabs

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,14 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./styles/lovable.css";
 import { Toaster } from "./components/ui/sonner";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/700.css";
+import "@fontsource/volkhov/400.css";
+import "@fontsource/volkhov/700.css";
+import "./styles/lovable.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
-      <Toaster richColors position="top-right" />
+      <Toaster richColors position="bottom-right" />
     </BrowserRouter>
   </StrictMode>
 );
