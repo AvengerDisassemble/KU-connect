3d54f213e2f127d805939d942a1bc19e8deafe42
Nichapat Sirinantanakul <nichapat.sirin@ku.th>
2025-09-30 02:23:18 +0700

    feat(employer-pages): add JobListing and Settings placeholders

---
+++ frontend/src/pages/employer/SettingPage.tsx
@@ -0,0 +1,17 @@
+import EmployerSidebar from "@/components/EmployerSideBar";
+
+export default function EmployerSettingPage() {
+  return (
+    <>
+      <div className="fixed inset-0 -z-50 pointer-events-none bg-bg-1" />
+
+      <aside className="fixed inset-y-0 left-0 z-20 w-[280px]">
+        <EmployerSidebar />
+      </aside>
+
+      <main className="min-h-screen pl-[280px] p-6">
+        Emplyer Setting Page(placeholder)
+      </main>
+    </>
+  );
+}
