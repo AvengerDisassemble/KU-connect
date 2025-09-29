0c2ccda3ec2b5b11a8d5df60216e7d2e79b8c848
Phantawat LUEANGSIRIWATTANA <phantawat.l@ku.th>
2025-09-29 15:45:49 +0700

    feat(schema): add email, phone number to User with new enum classes and default industry/companySize with updatedAt timestamps

---
+++ backend/prisma/migrations/20250929084142_add_user_student_hr_fields/migration.sql
@@ -0,0 +1,80 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `HR` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `hrId` to the `Job` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Student` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `email` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_HR" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "userId" INTEGER NOT NULL,
+    "companyName" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "industry" TEXT NOT NULL DEFAULT 'OTHER',
+    "companySize" TEXT NOT NULL DEFAULT 'ONE_TO_TEN',
+    "website" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "HR_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_HR" ("address", "companyName", "id", "userId") SELECT "address", "companyName", "id", "userId" FROM "HR";
+DROP TABLE "HR";
+ALTER TABLE "new_HR" RENAME TO "HR";
+CREATE UNIQUE INDEX "HR_userId_key" ON "HR"("userId");
+CREATE TABLE "new_Job" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "hrId" INTEGER NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "application_deadline" DATETIME NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone_number" TEXT NOT NULL,
+    "other_contact_information" TEXT NOT NULL,
+    "requirements" TEXT NOT NULL,
+    CONSTRAINT "Job_hrId_fkey" FOREIGN KEY ("hrId") REFERENCES "HR" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Job" ("application_deadline", "description", "email", "id", "location", "other_contact_information", "phone_number", "requirements", "title") SELECT "application_deadline", "description", "email", "id", "location", "other_contact_information", "phone_number", "requirements", "title" FROM "Job";
+DROP TABLE "Job";
+ALTER TABLE "new_Job" RENAME TO "Job";
+CREATE TABLE "new_Student" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "userId" INTEGER NOT NULL,
+    "degreeTypeId" INTEGER NOT NULL,
+    "address" TEXT NOT NULL,
+    "gpa" REAL,
+    "expectedGraduationYear" INTEGER,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Student_degreeTypeId_fkey" FOREIGN KEY ("degreeTypeId") REFERENCES "DegreeType" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Student_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Student" ("address", "degreeTypeId", "id", "userId") SELECT "address", "degreeTypeId", "id", "userId" FROM "Student";
+DROP TABLE "Student";
+ALTER TABLE "new_Student" RENAME TO "Student";
+CREATE UNIQUE INDEX "Student_userId_key" ON "Student"("userId");
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "surname" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phoneNumber" TEXT,
+    "verified" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO "new_User" ("id", "name", "password", "surname", "username", "verified") SELECT "id", "name", "password", "surname", "username", "verified" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE UNIQUE INDEX "User_phoneNumber_key" ON "User"("phoneNumber");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
