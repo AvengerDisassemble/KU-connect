f079366d35b4de63dddf0c071f8a4b8505c092b2
Sunthorn Kompita <sunthorn.ko@ku.th>
2025-11-18 15:37:06 +0700

    feat(student): remove deprecated DashboardPage and related components for code cleanup

--- frontend/src/pages/student/testaetae/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { WelcomeHeader } from "./WelcomeHeader";
-export { QuickActionGrid } from "./QuickActionGrid";
-export { StatsSummary } from "./StatsSummary";
-export { RecentJobsCard } from "./RecentJobsCard";
-export { MyApplicationsCard } from "./MyApplicationsCard";
-export { DashboardSkeleton } from "./DashboardSkeleton";
-export { DashboardErrorState } from "./DashboardErrorState";
